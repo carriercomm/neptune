b72861b952cd71220db349cc2b5f705ff29e2aa7
Jason May <jason.a.may@gmail.com>
2013-02-18 11:09:16 -0500

    bring akka back into sbt to avoid typesafe resolution issues
    Also get a basic listening server working with akka.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "Neptune"
 version := "0.0.0"
 
 scalaVersion := "2.10.0"
+
+resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases"
+
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % "2.1.0"
--- src/main/scala/org/jarsonmar/Neptune.scala
+++ src/main/scala/org/jarsonmar/Neptune.scala
@@ -1,42 +1,28 @@
 package org.jarsonmar
 
-import java.io._
-
-import akka.actor.Actor
-import akka.actor.Props
-//import akka.pattern.ask
-//import akka.util.duration._
-//import akka.util.Timeout
-//
-//import akka.io.IO
-//import akka.io.Tcp._
+import akka.actor._
+import akka.util._
 
 
 //case object Tick
 //case object Get
 
-object Neptune extends App {
-  println("Hello world")
+object Neptune {
+  class Listener extends Actor {
+
+    override def preStart {
+      println("starting on port 6715")
+      IOManager(context.system).listen("localhost", 6715)
+    }
+
+    def receive = {
+      case IO.NewClient(socket) =>
+        println("aaaaaaaaaaaaaaaaahhhhhhhhhhhhhh")
+    }
+  }
 }
-//object Neptune extends App {
-//  val system = ActorSystem("Neptune")
-//  val server = system.actorOf(Props[Neptune.Server])
-//
-//  val listener = new ServerSocket(6715);
-//  server ! Bind(socket, "localhost")
-//
-//  //
-//  //  case count => println("Count is " + count)
-//  //}
-//
-//  system.shutdown()
-//
-//  class Server extends IO(Tcp) {
-//    var count = 0
-//
-//    def receive = {
-//      case Tick => count += 1
-//      case Get  => sender ! count
-//    }
-//  }
-//}
+
+object Main extends App {
+  val system = ActorSystem("Neptune")
+  val server = system.actorOf(Props[Neptune.Listener])
+}
