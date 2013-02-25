728792cd828cf00bcdaca98b30d591f0a6488a80
Jason May <jason.a.may@gmail.com>
2013-02-24 21:27:35 -0500

    import data massaged with python into redis

--- src/main/scala/org/jarsonmar/neptune/universe/Builder.scala
+++ src/main/scala/org/jarsonmar/neptune/universe/Builder.scala
@@ -10,47 +10,28 @@ class Builder {
     port = 6379
   )
 
-  private def eachFile(predicate: (String, Option[JSONType]) => Any) = {
-    val stream = getClass.getClassLoader.getResourceAsStream("zones/enabled_zones")
-    io.Source.fromInputStream(stream).getLines() foreach { zone =>
-      val zone_stream = getClass.getClassLoader.getResourceAsStream("zones/" + zone + ".json")
-      val json = io.Source.fromInputStream(zone_stream).mkString
-      val zone_data = JSON parseRaw json
-      predicate(zone, zone_data)
-    }
-  }
-
   def build = {
 
-    eachFile { (zone, zone_data) =>
-      zone_data map {
-        case JSONObject(obj) => obj.get("loc") map {
-          _.asInstanceOf[JSONObject].obj.toIterator foreach {
-            case (id, data_obj) =>
-            val loc_data = data_obj.asInstanceOf[JSONObject].obj
-            def loc_data_value(key: String): Any = loc_data.get(key) getOrElse {
-              throw new Exception(key + " required") // XXX
+    val stream = getClass.getClassLoader.getResourceAsStream("zones.json")
+    val json = io.Source.fromInputStream(stream).mkString
+    val zone_data = JSON parseRaw json
+    zone_data map {
+      _.asInstanceOf[JSONObject].obj.toIterator foreach {
+        case (key, data_value) => data_value match {
+          case JSONArray(l) => l foreach { e: Any =>
+            if (e.isInstanceOf[String]) { // would be null otherwise
+              redis.sadd(key, e.asInstanceOf[String])
             }
-            val title = loc_data_value("title").asInstanceOf[String]
-            val desc = loc_data_value("description").asInstanceOf[String]
-
-            val exit_data = loc_data.get("exits")
-                            .map(_.asInstanceOf[JSONObject].obj)
-                            .getOrElse(Map())
-
-            val Array(loc_id, loc_zone) = id.split("@", 2)
-            val redis_loc = new Redis.Location(redis, loc_zone, loc_id)
-            redis_loc.setTitle(title)
-            redis_loc.setDescription(desc)
-
-            for (exit <- List("n", "s", "e", "w", "u", "d")) yield {
-              exit_data get exit map {
-                exit_loc => redis_loc.setDefaultExit(exit, exit_loc.asInstanceOf[String])
+          }
+          case JSONObject(o) => o.toIterator foreach {
+            case (hash_field, hash_value) => {
+              if (hash_value.isInstanceOf[String]) {
+                redis.hset(key, hash_field, hash_value.asInstanceOf[String])
               }
             }
           }
+          case s: String => redis.set(key, s)
         }
-        case _ =>
       }
     }
   }
--- src/main/scala/org/jarsonmar/neptune/universe/Redis.scala
+++ src/main/scala/org/jarsonmar/neptune/universe/Redis.scala
@@ -9,42 +9,14 @@ class Redis(host: String = "localhost", port: Int = 6379) {
 
   def getClient = client
 
-  // internal method
-  private def ns(key: String) = key_namespace + ":" + key
-
   // key stuff
-  def set(key: String, value: String) = client.set(ns(key), value)
+  def set(key: String, value: String) = client.set(key, value)
 
   // hash stuff
-  def hset(key: String, field: String, value: String) = client.hset(ns(key), field, value)
-  def hget(key: String, field: String) = client.hget(ns(key), field)
+  def hset(key: String, field: String, value: String) = client.hset(key, field, value)
+  def hget(key: String, field: String) = client.hget(key, field)
 
   // set stuff
-  def sadd(key: String, value: String) = client.set(ns(key), value)
-  def smove(key: String, value: String) = client.set(ns(key), value)
-}
-
-object Redis {
-  class Location(redis: Redis, loc_zone: String, loc_id: String) {
-    private def ns(key: String) = loc_zone + ":" + loc_id + ":" + key
-
-    def setTitle(title: String) = redis.hset(ns("properties"), "title", title)
-    def getTitle(title: String): String = redis.hget(ns("properties"), "title")
-
-    def setDescription(title: String) = redis.hset(ns("properties"), "description", title)
-    def getDescription(title: String) = redis.hget(ns("properties"), "description")
-
-    def setDefaultExit(exit: String,
-      exit_loc: String) = redis.hset(ns("exits"), exit, exit_loc)
-    def getDefaultExit(exit: String): String = redis.hget(ns("defexits"), exit)
-    def setCurrentExit(exit: String,
-      exit_loc_type: String,
-      exit_loc_data: String) = {
-      redis.hset(ns("exits"), exit, exit_loc_type + ":" + exit_loc_data)
-    }
-  }
-
-  class Player(redis: Redis) {
-    // TODO use this to abstract away set logic for players
-  }
+  def sadd(key: String, value: String) = client.set(key, value)
+  def smove(key: String, value: String) = client.set(key, value)
 }
