393a2817fc73f6b2985677def662473c654a5e15
Jason May <jason.a.may@gmail.com>
2013-03-09 18:33:31 -0500

    abstract away some repetitious routines

--- util/massage.py
+++ util/massage.py
@@ -1,12 +1,32 @@
 import glob
 import json
 
+# global neptune namespace
+def g_namespace():
+    return 'org.jarsonmar.neptune'
+
+def set_nested_value(o, k, p, v):
+    if k not in o:
+        o[k] = {}
+    o[k][p] = v
+
+def add_to_value(o, k, v):
+    if k not in o:
+        o[k] = []
+    o[k].append(v)
+
+def add_to_nested_value(o, k, p, v):
+    if k not in o:
+        o[k] = {}
+    if p not in o[k]:
+        o[k][p] = []
+    o[k][p].append(v)
 
 def generate_data():
     iter = glob.glob('./zones/*.json')
 
-    # global neptune namespace
-    g_namespace = 'org.jarsonmar.neptune'
+    movables_key = "%s:%s" % (g_namespace(), 'movables')
+    hostiles_key = "%s:%s" % (g_namespace(), 'hostiles')
 
     out_data = {}
     combined = {"loc": {}, "obj": {}, "mob": {}}
@@ -19,16 +39,16 @@ def generate_data():
     for loc_id, loc_data, in locations.iteritems():
         local_id, zone = loc_id.split('@')
 
-        ns = lambda key: "%s:loc:%s:%s:%s" % (g_namespace, zone, local_id, key)
+        ns = lambda key: "%s:loc:%s:%s:%s" % (g_namespace(), zone, local_id, key)
 
         if ns("properties") not in out_data:
             out_data[ns("properties")] = {}
 
-        out_data[ns("properties")]["title"]       = loc_data["title"]
-        out_data[ns("properties")]["description"] = loc_data["description"]
-        out_data[ns("properties")]["altitude"]    = loc_data["altitude"]
+        for data_key in ["title", "description", "altitude"]:
+            add_to_nested_value(out_data, ns("properties"), data_key, loc_data[data_key])
 
-        out_data[ns("flags")] = loc_data["flags"]
+        if "flags" in loc_data:
+            out_data[ns("flags")] = loc_data["flags"]
 
         for direction in ["n", "s", "e", "w", "u", "d"]:
             if direction not in loc_data["exits"]:
@@ -38,10 +58,8 @@ def generate_data():
             exit_value = ensure_zone(ex.lstrip("^"), zone)
 
             if ex.startswith("^"):
-                object_link_key = "%s:%s:%s:%s:links" % (g_namespace, "obj", zone, local_id)
-                if object_link_key not in out_data:
-                    out_data[object_link_key] = {}
-                out_data[object_link_key][direction] = exit_value
+                object_link_key = "%s:%s:%s:%s:links" % (g_namespace(), "obj", zone, local_id)
+                set_nested_value(out_data, object_link_key, direction, exit_value)
             else:
                 if ns("exits") not in out_data:
                     out_data[ns("exits")] = {}
@@ -51,7 +69,7 @@ def generate_data():
     for obj_id, obj_data, in objects.iteritems():
         local_id, zone = obj_id.split('@')
 
-        ns = lambda key: "%s:obj:%s:%s:%s" % (g_namespace, zone, local_id, key)
+        ns = lambda key: "%s:obj:%s:%s:%s" % (g_namespace(), zone, local_id, key)
 
         if ns("properties") not in out_data:
             out_data[ns("properties")] = {}
@@ -79,40 +97,33 @@ def generate_data():
             dest_value = ensure_zone(dest, zone)
 
             # These will be in redis sets
+            val = ':'.join([zone, local_id])
             if loc_type == "IN_ROOM":
-                room_key = ":".join([g_namespace, "loc", dest_value, "objs"])
-                if room_key not in out_data:
-                    out_data[room_key] = []
-                out_data[room_key].append(':'.join([zone, local_id]))
+                room_key = ":".join([g_namespace(), "loc", dest_value, "objs"])
+                add_to_value(out_data, room_key, val)
                 out_data[ns("location")] = dest_value
             elif loc_type == "IN_CONTAINER":
-                cont_key = ":".join([g_namespace, "obj", dest_value, "containing"])
-                if cont_key not in out_data:
-                    out_data[cont_key] = []
-                out_data[cont_key].append(':'.join([zone, local_id]))
+                cont_key = ":".join([g_namespace(), "obj", dest_value, "containing"])
+                add_to_value(out_data, cont_key, val)
                 out_data[ns("container")] = dest_value
             elif loc_type == "WIELDED_BY":
-                wield_key = ":".join([g_namespace, "mob", dest_value, "wielding"])
-                out_data[wield_key] = ':'.join([zone, local_id]) # only one at a time
+                wield_key = ":".join([g_namespace(), "mob", dest_value, "wielding"])
+                out_data[wield_key] = val # only one at a time
                 out_data[ns("wieldedby")] = dest_value
             elif loc_type == "WORN_BY":
-                wear_key = ":".join([g_namespace, "mob", dest_value, "wearing"])
-                if wear_key not in out_data:
-                    out_data[wear_key] = []
-                out_data[wear_key].append(':'.join([zone, local_id]))
+                wear_key = ":".join([g_namespace(), "mob", dest_value, "wearing"])
+                add_to_value(out_data, wear_key, val)
                 out_data[ns("wornby")] = dest_value
             elif loc_type == "CARRIED_BY":
-                carry_key = ":".join([g_namespace, "mob", dest_value, "carrying"])
-                if carry_key not in out_data:
-                    out_data[carry_key] = []
-                out_data[carry_key].append(':'.join([zone, local_id]))
+                carry_key = ":".join([g_namespace(), "mob", dest_value, "carrying"])
+                add_to_value(out_data, carry_key, val)
                 out_data[ns("carriedby")] = dest_value
 
     mobiles = combined["mob"]
     for mob_id, mob_data, in mobiles.iteritems():
         local_id, zone = mob_id.split('@')
 
-        ns = lambda key: "%s:mob:%s:%s:%s" % (g_namespace, zone, local_id, key)
+        ns = lambda key: "%s:mob:%s:%s:%s" % (g_namespace(), zone, local_id, key)
 
         if ns("properties") not in out_data:
             out_data[ns("properties")] = {}
@@ -125,7 +136,7 @@ def generate_data():
                     dest = mob_data["location"]
                     dest_value = ensure_zone(dest, zone)
 
-                    room_key = ":".join([g_namespace, "loc", dest_value, "mobs"])
+                    room_key = ":".join([g_namespace(), "loc", dest_value, "mobs"])
                     if room_key not in out_data:
                         out_data[room_key] = []
                     out_data[room_key].append(':'.join([zone, local_id]))
